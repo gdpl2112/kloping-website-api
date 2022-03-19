e7fadd1eb778f6f6b8de97dc68798c6159c2f38c
HRS-Computer <3474006766@qq.com>
2022-03-19 18:35:04 +0800

    update Time On time 22/3/19

--- src/main/java/io/github/kloping/mywebsite/controller/SystemController.java
+++ src/main/java/io/github/kloping/mywebsite/controller/SystemController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static io.github.kloping.mywebsite.Source.println;
 
