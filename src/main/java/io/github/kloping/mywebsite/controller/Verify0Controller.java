56d6f6253369c1abf25162378afcb21db328cf65
HRS-Computer <3474006766@qq.com>
2022-05-17 15:00:40 +0800

    update

--- src/main/java/io/github/kloping/mywebsite/controller/Verify0Controller.java
+++ src/main/java/io/github/kloping/mywebsite/controller/Verify0Controller.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -19,7 +20,28 @@ import java.util.UUID;
  * @author github.kloping
  */
 @RestController
-public class Verify0Controller {
+public class Verify0Controller implements InterceptorBroadcast.InterceptorReceiver {
+
+    public Verify0Controller() {
+        InterceptorBroadcast.INSTANCE.add(this);
+    }
+
+    @Override
+    public synchronized boolean onReceive(String ip, String url, Map<String, String[]> map, HttpServletRequest request) {
+        if (url.endsWith(".jar")) {
+            String fn = url.substring(url.lastIndexOf("/") + 1).trim();
+            if (FN2NUM.containsKey(fn)) {
+                FN2NUM.put(fn, FN2NUM.get(fn) + 1);
+            }
+            if (FN2NUM.get(fn) >= 3) {
+                new File(SOURCE_DIR, fn).delete();
+                FN2NUM.remove(fn);
+                CODE2FN.remove(FN2CODE.get(fn));
+                FN2CODE.remove(fn);
+            }
+        }
+        return false;
+    }
 
     @Autowired
     Verify0Mapper mapper;
@@ -35,24 +57,24 @@ public class Verify0Controller {
 
     public static final File SOURCE_DIR = new File("./files");
 
+    public static final Map<String, Integer> FN2NUM = new HashMap<>();
+    public static final Map<String, String> FN2CODE = new HashMap<>();
+    public static final Map<String, String> CODE2FN = new HashMap<>();
+
     @RequestMapping("/verify1")
     public String v1(@RequestParam("code") String code) throws IOException {
         if (v0(code)) {
+            if (CODE2FN.containsKey(code)) {
+                return CODE2FN.get(code);
+            }
             String uuid = UUID.randomUUID().toString();
             String fn = uuid + ".jar";
             File source = new File("./M1.jar");
             File target = new File(SOURCE_DIR, fn);
             MyUtils.copyFileUsingStream(source, target);
-            InterceptorBroadcast.INSTANCE.add(new InterceptorBroadcast.OnceInterceptorReceiver() {
-                @Override
-                public boolean onReceive(String ip, String url, Map<String, String[]> map, HttpServletRequest request) {
-                    if (url.contains(fn)) {
-                        target.delete();
-                        return true;
-                    }
-                    return false;
-                }
-            });
+            FN2NUM.put(fn, 0);
+            CODE2FN.put(code, fn);
+            FN2CODE.put(fn, code);
             return fn;
         } else {
             return null;
