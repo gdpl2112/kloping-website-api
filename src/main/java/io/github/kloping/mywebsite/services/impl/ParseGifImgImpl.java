dbc00a9c3c63fab36841ac59a7cebfee5eea1121
HRS-Computer <3474006766@qq.com>
2022-06-05 13:21:15 +0800

    update

--- src/main/java/io/github/kloping/mywebsite/services/impl/ParseGifImgImpl.java
+++ src/main/java/io/github/kloping/mywebsite/services/impl/ParseGifImgImpl.java
@@ -8,28 +8,35 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author github-kloping
+ */
 @Service
 public class ParseGifImgImpl implements IParseImg {
     @Override
     public String[] parse(String url) throws Exception {
         Connection connection = Jsoup.connect(url)
                 .ignoreContentType(true)
-                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50");
+                .userAgent("Mozilla/5.0 (Linux; U; Android 12; zh-CN; PEGT10 Build/RKQ1.211103.002) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.108 Quark/5.7.0.213 Mobile Safari/537.36");
         Document doc = connection.get();
 
-        Elements elements = doc.getElementsByTag("img");
+        Element e0 = doc.getElementsByClass("swiper-wrapper").get(0);
+
+        Elements elements = e0.getElementsByTag("img");
 
         Elements org_eles = new Elements();
 
         for (Element element : elements) {
-            String classStr = element.attr("class");
-            if (classStr.equals("long-mode-item"))
-                org_eles.add(element);
+            org_eles.add(element);
         }
         String[] strings = new String[org_eles.size()];
         int n = 0;
         for (Element element : org_eles) {
-            strings[n++] = element.attr("src");
+            String i0 = element.attr("src");
+            if (!i0.startsWith("http")) {
+                i0 = "https:" + i0;
+            }
+            strings[n++] = i0;
         }
         return strings;
     }
