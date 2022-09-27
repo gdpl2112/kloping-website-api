48b19bb95b1304a6aa114a2b173ab482d2bd58a2
HRS-Computer <3474006766@qq.com>
2022-09-27 12:15:06 +0800

    update

--- src/main/resources/static/js/player.js
+++ src/main/resources/static/js/player.js
@@ -1,22 +1,22 @@
 // <audio style="display: none;" autoPlay="autoplay" loop="loop" preload="auto" controls="controls">
 //     <source src="https://api.uomg.com/api/rand.music?sort=热歌榜" type="audio/mpeg">
 // </audio>
-let audio = document.createElement("audio")
-audio.style.display = "none"
-audio.autoplay = true
-audio.controls = true
-let source0 = document.createElement("source")
-source0.src = "https://api.uomg.com/api/rand.music?sort=热歌榜"
-source0.type = "audio/mpeg"
-audio.appendChild(source0)
-audio.addEventListener("loadeddata", function () {
-    audio.play().then(r => function () {
-
-    })
-})
-audio.addEventListener("ended", function () {
-    source0.src = "https://api.uomg.com/api/rand.music?sort=热歌榜"
-    source0.type = "audio/mpeg"
-    audio.load()
-})
-document.getElementsByTagName("body")[0].appendChild(audio)
+// let audio = document.createElement("audio")
+// audio.style.display = "none"
+// audio.autoplay = true
+// audio.controls = true
+// let source0 = document.createElement("source")
+// source0.src = "https://api.uomg.com/api/rand.music?sort=热歌榜"
+// source0.type = "audio/mpeg"
+// audio.appendChild(source0)
+// audio.addEventListener("loadeddata", function () {
+//     audio.play().then(r => function () {
+//
+//     })
+// })
+// audio.addEventListener("ended", function () {
+//     source0.src = "https://api.uomg.com/api/rand.music?sort=热歌榜"
+//     source0.type = "audio/mpeg"
+//     audio.load()
+// })
+// document.getElementsByTagName("body")[0].appendChild(audio)
