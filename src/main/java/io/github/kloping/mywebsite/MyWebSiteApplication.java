caf3802e7e040eabff548645693d32a64eb37ddc
Kloping <3474006766@qq.com>
2023-09-05 22:37:37 +0800

    fix bug

--- src/main/java/io/github/kloping/mywebsite/MyWebSiteApplication.java
+++ src/main/java/io/github/kloping/mywebsite/MyWebSiteApplication.java
@@ -36,7 +36,7 @@ public class MyWebSiteApplication {
         onCreate();
         applicationContext = SpringApplication.run(MyWebSiteApplication.class, args);
         io.github.kloping.mywebsite.plugins.Source.before();
-        System.out.println("start succes -v 7-21");
+        System.out.println("start succes -v 9-5");
 
         String name = ManagementFactory.getRuntimeMXBean().getName();
         String pid = name.split("@")[0];
