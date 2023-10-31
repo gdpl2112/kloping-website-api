c1417e0b6c73140831f7858846e38c75bbbc2362
Kloping <3474006766@qq.com>
2023-10-31 20:32:52 +0800

    update

--- src/main/java/io/github/kloping/mywebsite/controller/ApiTempController.java
+++ src/main/java/io/github/kloping/mywebsite/controller/ApiTempController.java
@@ -1,6 +1,12 @@
 package io.github.kloping.mywebsite.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import io.github.kloping.mywebsite.entitys.medias.Song;
+import io.github.kloping.mywebsite.entitys.medias.Songs;
 import io.github.kloping.mywebsite.services.ChatBotService;
+import io.github.kloping.mywebsite.services.impl.SearchSongKugou;
+import io.github.kloping.url.UrlUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author github.kloping
@@ -45,5 +53,43 @@ public class ApiTempController {
     @RequestMapping("/test")
     public String test(HttpServletRequest request) {
         return UtilsController.getHost(request);
+    }
+
+    @Autowired
+    SearchSongKugou searchSongKugou;
+
+
+    private List<JSONObject> tempList = new ArrayList<>();
+
+    private int index0 = 1;
+    private int indexMax = 20;
+
+    @RequestMapping("/get-music")
+    public Object getMusic() {
+        try {
+            if (tempList.isEmpty() || index0++ % indexMax == 0) {
+                String data = UrlUtils.getStringFromHttpUrl("http://localhost/get?pwd=r&key=songs");
+                for (Object o : JSONArray.parseArray(data)) {
+                    String name = o.toString();
+                    Songs songs = searchSongKugou.searchSong(name, 1);
+                    Song song = songs.getData()[0];
+                    JSONObject jo = new JSONObject();
+                    jo.put("name", song.getMedia_name());
+                    jo.put("artist", song.getAuthor_name());
+                    jo.put("cover", song.getImgUrl());
+                    jo.put("url", song.getSongUrl());
+                    tempList.add(jo);
+                }
+            }
+            return tempList;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping("/clear-music")
+    public void clearMusic() {
+        tempList.clear();
     }
 }
