7c10824cbc178760f3681ad8bb83bc1b73fc62e9
Kloping <3474006766@qq.com>
2023-12-22 10:15:41 +0800

    feat

--- src/main/java/io/github/kloping/mywebsite/MyWebSiteApplication.java
+++ src/main/java/io/github/kloping/mywebsite/MyWebSiteApplication.java
@@ -9,8 +9,14 @@ import io.github.kloping.file.FileUtils;
 import io.github.kloping.mywebsite.broadcast.EmailReceivesBroadcast;
 import io.github.kloping.mywebsite.plugins.PluginsSource;
 import io.github.kloping.mywebsite.webhook.WebHookStarter;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -91,4 +97,34 @@ public class MyWebSiteApplication {
         defaultKaptcha.setConfig(config);
         return defaultKaptcha;
     }
+
+    /**
+     * http自动跳转https
+     */
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8801); // 原http端口
+        connector.setSecure(false);
+        connector.setRedirectPort(8443); // 跳转的https端口，也就是我们配置文件中配置的项目端口
+        return connector;
+    }
+
 }
